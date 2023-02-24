647855388a89f3bed16ebfc4068088087df09a35
nexarchos <nexar001@fiu.edu>
2023-02-24 12:24:41 -0500

    Added bereal files to repository

---
+++ BeReal/Feed/FeedViewController.swift
@@ -0,0 +1,89 @@
+//
+//  FeedViewController.swift
+//  BeReal
+//
+//  Created by Nicole Exarchos on 02/20/23.
+//
+
+import UIKit
+
+// TODO: Import Parse Swift
+
+import ParseSwift
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    private let refreshControl = UIRefreshControl()
+    
+    private var posts = [Post]() {
+        didSet {
+            // Reload table view data any time the posts variable gets updated.
+            tableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        
+        tableView.refreshControl = refreshControl
+//        refreshControl.addTarget(self, action: #selector(onPullToRefresh), for: .valueChanged)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        queryPosts()
+    }
+
+    private func queryPosts(completion: (() -> Void)? = nil) {
+        // TODO: Pt 1 - Query Posts
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/2%20-%20Finding%20Objects.xcplaygroundpage/Contents.swift#L66
+
+        let query = Post.query()
+            .include("user")
+            .order([.descending("createdAt")])
+
+        // Fetch objects (posts) defined in query (async)
+        query.find { [weak self] result in
+            switch result {
+            case .success(let posts):
+                // Update local posts property with fetched posts
+                self?.posts = posts
+            case .failure(let error):
+                self?.showAlert(description: error.localizedDescription)
+            }
+            
+            completion?()
+        }
+        
+    }
+
+    
+    private func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: posts[indexPath.row])
+        return cell
+    }
+}
+
+extension FeedViewController: UITableViewDelegate { }
